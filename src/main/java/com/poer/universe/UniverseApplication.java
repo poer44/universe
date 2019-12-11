58875ffda2019845af33c127cb6698552b46dcd7
xuebw <Poer666>
2019-12-11 11:36:46 +0800

    Initial commit

---
+++ src/main/java/com/poer/universe/UniverseApplication.java
@@ -0,0 +1,14 @@
+package com.poer.universe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniverseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniverseApplication.class, args);
+        System.out.println("Hello");
+    }
+
+}
---
+++ src/test/java/com/poer/universe/UniverseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poer.universe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniverseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
